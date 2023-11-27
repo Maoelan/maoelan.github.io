357dbb96db98a4383aebbe7e7fe5b4be5e5e3116
Maulana Muhammad <maulanamuhammad2000@gmail.com>
2023-11-27 18:23:00 +0700

    Fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,43 @@
 import { Link } from "react-router-dom";
 import "./NavbarStyle.css";
-import React from 'react';
-import { FaBars } from "react-icons/fa";
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-  return (
-      <div class="header">
-          <Link to="/">
-              <h1>Portofolio</h1>
-          </Link>
-          <ul className="nav-menu">
-              <li>
-                  <Link to="/">Home</Link>
-              </li>
-              <li>
-                  <Link to="/project">Project</Link>
-              </li>
-              <li>
-                  <Link to="/certificate">Certificate</Link>
-              </li>
-              <li>
-                  <Link to="/about">About</Link>
-              </li>
-              <li>
-                  <Link to="/contact">Contact</Link>
-              </li>
-          </ul>
-          <div className="hamburger">
-              <FaBars size={20} style={{ color: "#fff" }}/>
-          </div>
-      </div>
-  )
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+    return (
+        <div class="header">
+            <Link to="/">
+                <h1>Portofolio</h1>
+            </Link>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/project">Project</Link>
+                </li>
+                <li>
+                    <Link to="/certificate">Certificate</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+            <div className="hamburger" onClick={handleClick}>
+                {click ? (
+                    <FaTimes size={20} style={{ color: "#fff" }} />
+                ) : (
+                    <FaBars size={20} style={{ color: "#fff" }} />)}
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
